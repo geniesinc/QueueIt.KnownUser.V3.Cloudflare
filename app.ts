0b21872b3efb753dd8eed53b3b52a58916145dd8
Nick Mandel <nick.mandel@genies.com>
2022-05-04 18:51:18 -0700

    handling bypassing with cloudflare worker env variables

--- app.ts
+++ app.ts
@@ -1,7 +1,13 @@
 'use strict'
-require("dotenv").config();
+
+declare global {
+  const QUEUE_IT_SECRET_KEY: string
+  const BYPASS_STAGING: string
+  const BYPASS_PROD: string
+}
+
 const QUEUEIT_CUSTOMERID = "genies";
-const QUEUEIT_SECRETKEY = process.env.QUEUE_IT_SECRET_KEY ?? "";
+const QUEUEIT_SECRETKEY = QUEUE_IT_SECRET_KEY ?? "";
 
 // Set to true, if you have any trigger(s) containing experimental 'RequestBody' condition.
 const READ_REQUEST_BODY = false;
@@ -18,18 +24,20 @@ const handleRequest = async function (event: any) {
   const {request} = event;
   const handler = new QueueITRequestResponseHandler(QUEUEIT_CUSTOMERID, QUEUEIT_SECRETKEY, READ_REQUEST_BODY);
 
-  // exit early for other apis
-  // if (!request.url.includes("reserveNFT")) {
-  //   const response = await fetch(request);
-  //   return await handler.onClientResponse(response);
-  // }
+  // exit early based on env variables
+  if (
+    (request.url.includes('staging') && BYPASS_STAGING === 'true') ||
+    (!request.url.includes('staging') && BYPASS_PROD === 'true')
+  ) {
+    const response = await fetch(request);
+    return await handler.onClientResponse(response);
+  }
 
   let queueitResponse = await handler.onClientRequest(request);
   if (queueitResponse) {
     //it is a redirect- break the flow
     return await handler.onClientResponse(queueitResponse);
-  }
-  else {
+  } else {
     //call backend
     const response = await fetch(request);
     return await handler.onClientResponse(response);
